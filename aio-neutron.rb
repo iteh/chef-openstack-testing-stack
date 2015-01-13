b98b19328cfe717833f3b1db9cc37ce64673da71
Jürgen Brüder <juergen.brueder@gmail.com>
2015-01-13 11:41:21 +0100

    adjusting to new scenario

--- aio-neutron.rb
+++ aio-neutron.rb
@@ -1,23 +1,13 @@
-require 'chef/provisioning'
+require 'chef_metal_ssh'
 
-controller_config = <<-ENDCONFIG
-  config.vm.network "forwarded_port", guest: 443, host: 9443
-  config.vm.network "forwarded_port", guest: 4002, host: 4002
-  config.vm.network "forwarded_port", guest: 5000, host: 5000
-  config.vm.network "forwarded_port", guest: 8773, host: 8773
-  config.vm.network "forwarded_port", guest: 8774, host: 8774
-  config.vm.network "forwarded_port", guest: 35357, host: 35357
-  config.vm.provider "virtualbox" do |v|
-    v.memory = 4096
-    v.cpus = 2
-    v.customize ["modifyvm", :id, "--nicpromisc2", "allow-all"]
-    v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
-  end
-  config.vm.network "private_network", ip: "192.168.100.60"
-ENDCONFIG
+with_ssh_cluster("~/metal_ssh")
 
-machine 'controller' do
-  add_machine_options :vagrant_config => controller_config
+machine "controller" do
+  action [:ready, :converge]
+  machine_options 'ip_address' => '192.168.1.6', 'ssh_options' => {
+    'user' => 'root',
+    'password' => 'secret'
+  }
   role 'allinone-compute'
   role 'os-image-upload'
   recipe 'openstack-common::openrc'
